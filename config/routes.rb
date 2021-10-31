4504b1d4b14bc8775c55b31bf42b2e924f220db5
weichenzhao <weichenzhao@tencent.com>
2021-10-30 23:40:01 -0400

    Add base page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  root 'static_pages#home'
+  get 'home' => 'static_pages#home'
+  get 'about' => 'static_pages#about'
   resources :messages
   resources :transactions
   resources :categories
   resources :photos
   resources :items
   resources :users
+  get    'login'   => 'sessions#new' 
+  post   'login'   => 'sessions#create' 
+  delete 'logout'  => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
