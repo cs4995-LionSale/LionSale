55ee35285fab8bba63a6f08c2141517075cf3b2c
weichenzhao <weichenzhao@tencent.com>
2021-10-31 22:51:00 -0400

    Update user foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_one_attached :avatar
     has_many :messgaes_sent, class_name: 'Message', inverse_of: 'from'
     has_many :messgaes_received, class_name: 'Message', inverse_of: 'to'
-    has_many :items_sold, class_name: 'Item', inverse_of: 'seller'
-    has_many :items_bought, class_name: 'Item', inverse_of: 'buyer'
+    has_many :transactions_as_seller, class_name: 'Transaction', inverse_of: 'seller'
+    has_many :transactions_as_buyer, class_name: 'Transaction', inverse_of: 'buyer'
+    has_many :items_sold, class_name: 'Iten', inverse_of: 'seller'
 end
