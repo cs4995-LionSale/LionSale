7b096279417a01b07fb3f0ddfe2d4503ef357146
Alex Huang <alex.huang@wustl.edu>
2021-11-06 21:06:48 -0400

    some changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,8 @@ class ActiveSupport::TestCase
   def log_in_as(user, options = {})
     password    = options[:password]    || 'password'
     remember_me = options[:remember_me] || '1'
- if integration_test?      post login_path, session: { email:       user.email,
+ if integration_test?      
+  post login_path, session: { email:       user.email,
                                   password:    password,
                                   remember_me: remember_me }
     else
