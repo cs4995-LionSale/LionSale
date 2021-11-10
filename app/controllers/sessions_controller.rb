aa9300256ec24634bd05b243976d1c51619274f0
weichenzhao <weichenzhao@tencent.com>
2021-11-09 21:45:07 -0500

    Fix on minor things

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    # success = verify_recaptcha(action: 'login', minimum_score: 0.5, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
-    # checkbox_success = verify_recaptcha unless success
-    # if success || checkbox_success
+    success = verify_recaptcha(action: 'login', minimum_score: 0.5, secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
+    checkbox_success = verify_recaptcha unless success
+    if success || checkbox_success
       user = User.find_by(email: params[:email].downcase) 
       if user && user.authenticate(params[:password])    
       # login as user and redirect to user's profile 
@@ -13,12 +13,12 @@ class SessionsController < ApplicationController
         flash.now[:danger] = 'Email or Password is incorrect, please try again' 
         render 'new'
       end
-    # else
-    #   if !success
-    #     @show_checkbox_recaptcha = true
-    #   end
-    #   render 'new'
-    # end
+    else
+      if !success
+        @show_checkbox_recaptcha = true
+      end
+      render 'new'
+    end
   end
   
   def new
