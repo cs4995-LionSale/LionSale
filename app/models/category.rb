cd7f1bfc2da69271e9712eb513b4f51b405ac582
weichenzhao <weichenzhao@tencent.com>
2021-11-17 00:19:08 -0500

    Add category helpers

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,24 @@ class Category < ApplicationRecord
                           foreign_key: "parent_category_id"
   has_many :items, class_name: "Item", inverse_of: 'category', foreign_key: 'category_id'
   belongs_to :parent_category, class_name: "Category", optional: true, foreign_key: "parent_category_id"
+
+  def get_category_path
+    r = [self]
+    curr = parent_category
+    while curr do
+      r = r.push[curr]
+      curr = curr.parent_category
+    end
+    return r
+  end
+
+  def get_sub_categories(r)
+    r = r.push(self)
+    if subcatgories
+      subcatgories.each do |subcat|
+        r = r + subcat.get_sub_categories(r)
+      end
+    end
+    return r
+  end
 end
